51b0c3e2779ecfe97d75fb73fa71eafc862f82d9
erick <erik_pizani@hotmail.com>
2015-10-26 15:57:59 -0200

    teste

---
+++ horabella/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  horabella
+//
+//  Created by Erick Leal on 26/10/15.
+//  Copyright © 2015 Yi Mobile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ horabellaTests/horabellaTests.swift
@@ -0,0 +1,36 @@
+//
+//  horabellaTests.swift
+//  horabellaTests
+//
+//  Created by Erick Leal on 26/10/15.
+//  Copyright © 2015 Yi Mobile. All rights reserved.
+//
+
+import XCTest
+@testable import horabella
+
+class horabellaTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
