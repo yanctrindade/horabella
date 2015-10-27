a5000d7ecf397701c5fe3d69d1aa7227930aa971
erick <erik_pizani@hotmail.com>
2015-10-27 14:45:18 -0200

    textfields para login e cadastro e RatingView

--- horabella/LogInViewController.swift
+++ horabella/LogInViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class LogInViewController: UIViewController {
+    
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +23,14 @@ class LogInViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: - LogIn
+    
+    @IBAction func logIn(sender: AnyObject) {
+        
+        print("faz login")
+        
     }
     
 
--- horabella/SignUpTableViewController.swift
+++ horabella/SignUpTableViewController.swift
@@ -8,10 +8,24 @@
 
 import UIKit
 
-class SignUpTableViewController: UITableViewController {
-
+class SignUpTableViewController: UITableViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var birthdateTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstNameTextField.delegate = self
+        lastNameTextField.delegate = self
+        emailTextField.delegate = self
+        phoneTextField.delegate = self
+        birthdateTextField.delegate = self
+        passwordTextField.delegate = self
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -19,6 +33,119 @@ class SignUpTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
+    // MARK: - SignUp
+    
+    
+    @IBAction func facebookSignUp(sender: AnyObject) {
+        
+        print("cadastro com facebook")
+        
+    }
+    
+    @IBAction func signUp(sender: AnyObject) {
+        
+        if firstNameTextField.text!.characters.count < 3 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Nome muito curto!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.firstNameTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else if lastNameTextField.text!.characters.count < 3 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Sobrenome muito curto!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.lastNameTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else if emailTextField.text!.characters.count < 6 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Email muito curto!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.emailTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else if phoneTextField.text!.characters.count < 8 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Telefone muito curto!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.phoneTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else if birthdateTextField.text!.characters.count < 1 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Insira sua data de nascimento!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.birthdateTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else if passwordTextField.text!.characters.count < 6 {
+            
+            let alert = UIAlertController(title: "Erro", message: "Senha muito curta!", preferredStyle: UIAlertControllerStyle.Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: { (alert: UIAlertAction) -> Void in
+                self.passwordTextField.becomeFirstResponder()
+            }))
+            presentViewController(alert, animated: true, completion: nil)
+            
+        }else{
+            
+            print("faz cadastro")
+            
+        }
+        
+    }
+    
+    
+    // MARK: - DatePicker
+    
+    @IBAction func dateEditing(sender: UITextField) {
+        
+        let datePickerView:UIDatePicker = UIDatePicker()
+        
+        datePickerView.datePickerMode = UIDatePickerMode.Date
+        datePickerView.maximumDate = NSDate()
+        
+        sender.inputView = datePickerView
+        
+        datePickerView.addTarget(self, action: Selector("datePickerValueChanged:"), forControlEvents: UIControlEvents.ValueChanged)
+        
+    }
+    
+    func datePickerValueChanged(sender:UIDatePicker) {
+        
+        let dateFormatter = NSDateFormatter()
+        
+        dateFormatter.dateStyle = NSDateFormatterStyle.MediumStyle
+        
+        dateFormatter.timeStyle = NSDateFormatterStyle.NoStyle
+        
+        birthdateTextField.text = dateFormatter.stringFromDate(sender.date)
+        
+    }
+    
+    // MARK: - TextField
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        if textField == firstNameTextField {
+            lastNameTextField.becomeFirstResponder()
+        }else if textField == lastNameTextField {
+            emailTextField.becomeFirstResponder()
+        }else if textField == emailTextField {
+            phoneTextField.becomeFirstResponder()
+        }else if textField == passwordTextField {
+            passwordTextField.resignFirstResponder()
+        }
+        
+        return true
+    }
+    
 
 //    override func didReceiveMemoryWarning() {
 //        super.didReceiveMemoryWarning()
