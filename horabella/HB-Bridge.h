3275bce79c210e44d6cd647f39975025926aa73e
Yan Trindade <trindade.yan@gmail.com>
2015-10-27 14:53:01 -0200

    nem sei qq eh

---
+++ horabella/HB-Bridge.h
@@ -0,0 +1,13 @@
+//
+//  HB-Bridge.h
+//  horabella
+//
+//  Created by Yan Correa Trindade on 10/27/15.
+//  Copyright © 2015 Yi Mobile. All rights reserved.
+//
+
+#ifndef HB_Bridge_h
+#define HB_Bridge_h
+
+
+#endif /* HB_Bridge_h */
