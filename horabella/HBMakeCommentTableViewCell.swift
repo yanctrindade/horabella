cb8f5bb656792e58ae92fa8235a44fbffea026a6
erick <erik_pizani@hotmail.com>
2015-10-29 14:24:21 -0200

    novas classes

---
+++ horabella/HBMakeCommentTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  HBMakeCommentTableViewCell.swift
+//  horabella
+//
+//  Created by Erick Leal on 28/10/15.
+//  Copyright © 2015 Yi Mobile. All rights reserved.
+//
+
+import UIKit
+
+class HBMakeCommentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var comment: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @IBAction func sendComment(sender: AnyObject) {
+        
+        print("enviar comentario")
+        
+    }
+    
+
+}
