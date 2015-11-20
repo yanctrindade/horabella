2d53373628a0210cb0db9714a1e48ba8d5916b69
Yan Trindade <trindade.yan@gmail.com>
2015-11-20 17:30:09 -0200

    categorias carregando certo, faltam os servicos

---
+++ horabella/HBService.swift
@@ -0,0 +1,13 @@
+//
+//  HBService.swift
+//  horabella
+//
+//  Created by Yan Correa Trindade on 11/20/15.
+//  Copyright © 2015 Yi Mobile. All rights reserved.
+//
+
+import UIKit
+
+class HBService: NSObject {
+
+}
